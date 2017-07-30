f449060ff150554fb1791706dc620ec3666a98b3
Jim Vang <vangjim15@gmail.com>
2017-07-30 17:22:58 -0500

    added parent and child file for state and stateless

---
+++ Parent.js
@@ -0,0 +1,29 @@
+// Stateless components updating their parents' state is a React pattern
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Child } from './Child';
+
+class Parent extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { name: 'Frarthur' };
+
+    this.changeName = this.changeName.bind(this);
+  }
+
+  changeName(newName) {
+    this.setState({
+      name: newName
+    });
+  }
+
+  render() {
+    return <Child name={this.state.name} onChange={this.changeName} />
+  }
+}
+
+ReactDOM.render(
+	<Parent />,
+	document.getElementById('app')
+);
