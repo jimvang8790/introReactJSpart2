9234918b1db041bfb5d7a7fbff4fa16086ad2a9e
Jim Vang <vangjim15@gmail.com>
2017-07-30 16:40:55 -0500

    added parent stateful and child stateless

--- app.js
+++ app.js
@@ -1,2 +1,27 @@
 // Let's make a stateful component pass its state to a stateless component
 // To make that happen, you need two component classes: a stateful class, and a stateless class.
+
+// example made a stateful component class named Parent
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+class Parent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { name: 'Frarthur' };
+  }
+
+  render() {
+    return <div></div>;
+  }
+}
+
+// Now, let's make our stateless component class
+import React from 'react';
+
+export class Child extends React.Component {
+  render() {
+    return <h1>Hey, my name is {this.props.name}!</h1>;
+  }
+}
